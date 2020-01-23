468b3322c1efeeed8d163310714749c85420dc28
Sam Liu <luu.dat.tham@gmail.com>
2020-01-23 17:40:06 +0700

    update API key

--- src/constansts/config.js
+++ src/constansts/config.js
@@ -1,8 +1,8 @@
 export default {
-    API_KEY: "AIzaSyAuU7r7qzN_QurElvBvq5Bz-1QyF5LbFs8",
+    API_KEY: "AIzaSyDJjz3HjMpfXo6PZb9lWivVHOk8o52FcTY",
     spreadsheetId: "1SEylFu8T0XqIDJUJbRgtcl71MCGcY-pfnuXsu-cWrBI",
     mainUrl: "https://sheets.googleapis.com/v4/spreadsheets/1SEylFu8T0XqIDJUJbRgtcl71MCGcY-pfnuXsu-cWrBI/values:batchGet",
-    fullUrl: "https://sheets.googleapis.com/v4/spreadsheets/1SEylFu8T0XqIDJUJbRgtcl71MCGcY-pfnuXsu-cWrBI/values:batchGet?majorDimension=ROWS&key=AIzaSyAuU7r7qzN_QurElvBvq5Bz-1QyF5LbFs8&ranges=",
+    fullUrl: "https://sheets.googleapis.com/v4/spreadsheets/1SEylFu8T0XqIDJUJbRgtcl71MCGcY-pfnuXsu-cWrBI/values:batchGet?majorDimension=ROWS&key=AIzaSyDJjz3HjMpfXo6PZb9lWivVHOk8o52FcTY&ranges=",
     TABLE_WORDS: "words",
     TABLE_LESSONS: "lessons",
     TABLE_UPDATE_DATE: "update"
